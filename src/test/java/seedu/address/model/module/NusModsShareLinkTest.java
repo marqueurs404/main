bb3e92f52bdd8055ceac8f18d4bb69bab3bbdb1c
marqueurs404 <marcus.ong404@gmail.com>
2019-11-04 16:47:43 +0800

    Add tests for AddNusMod(s)Command and NusModsShareLink

--- src/test/java/seedu/address/model/module/NusModsShareLinkTest.java
+++ src/test/java/seedu/address/model/module/NusModsShareLinkTest.java
@@ -8,80 +8,73 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.testutil.moduleutil.NusModsShareLinkStrings;
 
 class NusModsShareLinkTest {
-    private String validLinkString1 = "https://nusmods.com/timetable/sem-1/share?CS2101=&CS2103T=LEC:G05&CS3230=LEC:1,"
-            + "TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    private String validLinkString2 = "https://nusmods.com/timetable/sem-1/share?CS2101=&CS2103T=LEC:G05&CS3230=LEC:1,"
-            + "TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    //Can't check validity of module code within parser as there's no access to model.
-    private String validLinkStringInvalidModuleCode = "https://nusmods.com/timetable/sem-1/share?ABCDEF=&"
-            + "CS2103T=LEC:G05&CS3230=LEC:1,TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    //Can't check validity of class number within parser as there's no access to model.
-    private String validLinkStringInvalidClassNumber = "https://nusmods.com/timetable/sem-1/share?CS2101=&CS2103T="
-            + "LEC:GXX&CS3230=LEC:1,TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    private String invalidLinkStringNotUrl = "not a url";
-    private String invalidLinkStringAnotherUrl = "https://google.com/";
-    private String invalidLinkStringTypoUrl = "https://nusbods.com/timetable/sem-1/share?CS2101=&CS2103T=LEC:G05&"
-            + "CS3230=LEC:1,TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    private String invalidLinkStringInvalidSemester = "https://nusmods.com/timetable/sem999/share?CS2101=&"
-            + "CS2103T=LEC:G05&CS3230=LEC:1,TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-    private String invalidLinkStringMissingQueryString = "https://nusmods.com/timetable/sem-1/share?";
-    private String invalidLinkStringInvalidQueryString = "https://nusmods.com/timetable/sem-1/share?CS2101";
-    private String invalidLinkStringInvalidClassType = "https://nusmods.com/timetable/sem-1/share?CS2101=&"
-            + "CS2103T=LOL:G05&CS3230=LEC:1,TUT:08&CS3243=TUT:07,LEC:1&GEQ1000=TUT:D17";
-
     @Test
     void parseLink_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> NusModsShareLink.parseLink(null));
     }
 
     @Test
+    void parseLink_validValues_noParseExceptionThrown() {
+        try {
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.VALID_LINK_1);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.VALID_LINK_2);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.VALID_LINK_INVALID_MODULE_CODE);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.VALID_LINK_INVALID_CLASS_NUMBER);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.VALID_LINK_DUPLICATE_MODULE_LESSONS);
+        } catch (ParseException e) {
+            assert false;
+        }
+    }
+
+    @Test
     void parseLink_invalidValues_throwsParseExceptionWithSpecificMessages() {
         try {
-            NusModsShareLink.parseLink(invalidLinkStringNotUrl);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_NOT_URL);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_CONSTRAINTS, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringAnotherUrl);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_ANOTHER_URL);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_CONSTRAINTS, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringTypoUrl);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_TYPO_URL);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_CONSTRAINTS, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringInvalidSemester);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_INVALID_SEMESTER);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_CONSTRAINTS, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringMissingQueryString);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_MISSING_QUERY_STRING);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_MISSING_QUERY_STRING, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringInvalidQueryString);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_INVALID_QUERY_STRING);
             assert false;
         } catch (ParseException e) {
             assertEquals(NusModsShareLink.MESSAGE_NOTHING_TO_ADD, e.getMessage());
         }
 
         try {
-            NusModsShareLink.parseLink(invalidLinkStringInvalidClassType);
+            NusModsShareLink.parseLink(NusModsShareLinkStrings.INVALID_LINK_INVALID_CLASS_TYPE);
             assert false;
         } catch (ParseException e) {
             assertTrue(e.getMessage().startsWith("Invalid class type"));
@@ -90,16 +83,17 @@ class NusModsShareLinkTest {
 
     @Test
     void isValidUrl() {
-        assertTrue(NusModsShareLink.isValidUrl(validLinkString1));
-        assertTrue(NusModsShareLink.isValidUrl(validLinkString2));
-        assertTrue(NusModsShareLink.isValidUrl(validLinkStringInvalidModuleCode));
-        assertTrue(NusModsShareLink.isValidUrl(validLinkStringInvalidClassNumber));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringNotUrl));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringAnotherUrl));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringTypoUrl));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringMissingQueryString));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringInvalidQueryString));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringInvalidSemester));
-        assertFalse(NusModsShareLink.isValidUrl(invalidLinkStringInvalidClassType));
+        assertTrue(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.VALID_LINK_1));
+        assertTrue(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.VALID_LINK_2));
+        assertTrue(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.VALID_LINK_INVALID_MODULE_CODE));
+        assertTrue(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.VALID_LINK_INVALID_CLASS_NUMBER));
+        assertTrue(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.VALID_LINK_DUPLICATE_MODULE_LESSONS));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_NOT_URL));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_ANOTHER_URL));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_TYPO_URL));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_MISSING_QUERY_STRING));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_INVALID_QUERY_STRING));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_INVALID_SEMESTER));
+        assertFalse(NusModsShareLink.isValidUrl(NusModsShareLinkStrings.INVALID_LINK_INVALID_CLASS_TYPE));
     }
 }
